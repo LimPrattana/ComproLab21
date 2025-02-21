05649c2966dc62f20d1c6c7f20a93640dd9083e4
LimPrattana <vivo.twy0270@gmail.com>
2025-02-21 12:37:12 +0700

    average completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+#include<cstdlib>
+#include<string>
+using namespace std;
+int main(int argc,char * argv[]){
 
+    if(argc<2){
+        cout<<"Please input numbers to find average.\n";
+        return 1;
+    }
+    cout<<"---------------------------------\n";
+    double sum = 0;
+    double avg;
+    int count = argc-1;
+    for(int i=1;i<argc;i++){
+        sum+= atof(argv[i]);
+    }
+    avg = sum/count;
+    cout<<"Average of "<<count<<" numbers = "<<avg;
+    cout<<"\n---------------------------------";
+return 0;
+}
